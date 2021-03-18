838f9410072ec6e8604bf3f4db2ae83ba0fe6d3e
Duke Yoo <dukuaris@gmail.com>
2021-03-18 17:03:34 +0900

    Prepar upload

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,9 @@ const message = document.getElementById('message');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    
     const location = search.value
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 message.innerText = data.error
